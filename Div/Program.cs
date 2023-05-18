36251739dbb37480b3d5900d07f85b51f17a99d0
Anton <B_atonyo_S@bk.ru>
2023-05-18 11:20:46 +0300

    Division

---
+++ Div/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите целые числа");
+double numberA = int.Parse(Console.ReadLine());
+double numberB = int.Parse(Console.ReadLine());
+Console.Write("При делении числа ");
+Console.Write(numberA);
+Console.Write(" на число ");
+Console.Write(numberB);
+Console.Write(" получится ");
+Console.WriteLine(numberA/numberB);
