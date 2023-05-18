97ffe77e2d5322f0786eb64c03a2685ec7b6a9c7
Anton <B_atonyo_S@bk.ru>
2023-05-18 11:03:10 +0300

    Sum

---
+++ Sum/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите два целых числа(через Enter)");
+int numberA = int.Parse(Console.ReadLine());
+int numberB = int.Parse(Console.ReadLine());
+Console.Write("Сумма числа ");
+Console.Write(numberA);
+Console.Write(" и числа ");
+Console.Write(numberB);
+Console.Write(" = ");
+Console.WriteLine(numberA+numberB);
+
