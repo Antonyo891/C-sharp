9e804e1f2448310b77f23cda4c3a02237c68a7f2
Anton <B_atonyo_S@bk.ru>
2023-05-18 12:01:48 +0300

    Hello Personal

---
+++ Hello_Personal/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Привет! Как тебя зовут?");
+string username = Console.ReadLine();
+if(username.ToLower()=="степан")
+{
+    Console.WriteLine("Ура!!! Это ведь Степан");
+    }
+else 
+{
+    Console.Write("Как жаль что это не степан(((...Привет, ");
+    Console.WriteLine(username);
+}
