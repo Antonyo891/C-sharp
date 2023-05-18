0ea462ae7c1755ff25ae3ed45cc603f5ba3a430a
Anton <B_atonyo_S@bk.ru>
2023-05-18 10:17:50 +0300

    Enter your name

--- Hello_User/Program.cs
+++ Hello_User/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine ("Enter your Name:");
 string username = Console.ReadLine();
 Console.WriteLine("Привет,");
 Console.WriteLine(username);
